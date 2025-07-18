f8df61eba51b8fdcd5308fd343ed48079806955a
Murtaza <murtazamandasore@gmail.com>
2025-07-18 16:07:01 +0500

    question 7 done

---
+++ question7.dart
@@ -0,0 +1,11 @@
+void main() {
+  /*
+  Check whether a number is even or not.
+   */
+  int number = 56;
+  if (number % 2 == 0) {
+    print("Number is even");
+  } else {
+    print("Number is odd");
+  }
+}
