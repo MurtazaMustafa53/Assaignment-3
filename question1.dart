78c217deaf1cba41f8e3819587a1ed6e5d274663
Murtaza <murtazamandasore@gmail.com>
2025-07-18 11:50:24 +0500

    question 1 done

--- question1.dart
+++ question1.dart
@@ -1,4 +1,12 @@
 void main() {
-    print("halo");
-  
+  /*
+Create two integer variables length and breadth and assign values then check if they are square values or rectangle values.
+ie: if both values are equal then it's square otherwise rectangle.
+*/
+  int length = 5, breadth = 8;
+  if (length == breadth) {
+    print("this shape is a square");
+  } else {
+    print("this shape is a rectangle");
+  }
 }
