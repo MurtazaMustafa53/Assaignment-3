27b82b59fa39e7ba3ae739db44f642308601a40f
Murtaza <murtazamandasore@gmail.com>
2025-07-18 16:11:15 +0500

    question 8 done

---
+++ question8.dart
@@ -0,0 +1,13 @@
+void main() {
+  /*
+  assume a number and check whether it is a multiple of 3 or 5.
+   */
+  int number = 54;
+  if (number % 3 == 0) {
+    print("The number is a multiple of 3");
+  } else if (number % 5 == 0) {
+    print("the number is a multiple of 5");
+  } else {
+    print("The number is not a multiple of 3 nor 5");
+  }
+}
