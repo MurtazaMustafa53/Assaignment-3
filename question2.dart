65d1232642b6f0e0524a8dad65d4372d68d8ebc1
Murtaza <murtazamandasore@gmail.com>
2025-07-18 11:54:23 +0500

    question 2 done

---
+++ question2.dart
@@ -0,0 +1,11 @@
+void main() {
+  /*
+  Take two variables and store age then using if/else condition to determine oldest and youngest among them.
+   */
+  int age1 = 13, age2 = 19;
+  if (age1 > age2) {
+    print("the first age is older and the second age is  younger");
+  } else {
+    print("the second age is the older and the second age is younger");
+  }
+}
